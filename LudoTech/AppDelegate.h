a8a8f2f7dab34adf941b0d47fa8060f91f4eefad
Valentin Bercot <valent1.bercot@gmail.com>
2015-03-20 16:45:44 +0100

    Initial Commit

---
+++ LudoTech/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  LudoTech
+//
+//  Created by Valentin Bercot on 20/03/2015.
+//  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ LudoTech/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LudoTech
+//
+//  Created by Valentin Bercot on 20/03/2015.
+//  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
