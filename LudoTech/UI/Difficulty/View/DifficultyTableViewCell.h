3e55994a2279de108c34db7728f4337fbcb4fbff
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-03-22 15:31:59 +0100

    Ajout DifficultyTableViewCell, Modification DifficultyTableViewController

---
+++ LudoTech/UI/Difficulty/View/DifficultyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DifficultyTableViewCell.h
+//  LudoTech
+//
+//  Created by remy on 22/03/2015.
+//  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DifficultyTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@end
---
+++ LudoTech/UI/Difficulty/View/DifficultyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DifficultyTableViewCell.m
+//  LudoTech
+//
+//  Created by remy on 22/03/2015.
+//  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
+//
+
+#import "DifficultyTableViewCell.h"
+
+@implementation DifficultyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
