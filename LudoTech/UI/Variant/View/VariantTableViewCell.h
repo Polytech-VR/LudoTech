f9d477d6bf0e9d24a3b457d453ceaf3a990274cb
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-03-25 09:47:16 +0100

    Ajout VariantTableViewCell

---
+++ LudoTech/UI/Variant/View/VariantTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  VariantTableViewCell.h
+//  LudoTech
+//
+//  Created by remy on 25/03/2015.
+//  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// ===== DEFINITION =====
+
+@interface VariantTableViewCell : UITableViewCell
+
+// ===== PROPERTIES =====
+
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *difficulty;
+
+@end
