79f548b4f5bcfcef5e561ff528064eac43662c25
infinity34 <remy@MacBook-Pro-de-Matthieu.local>
2015-03-25 08:59:53 +0100

    Ajout fonction d'ajout pour les langages

--- LudoTech/UI/Language/Add/AddLanguageViewController.m
+++ LudoTech/UI/Language/Add/AddLanguageViewController.m
@@ -5,10 +5,18 @@
 //  Created by Valentin Bercot on 18/03/2015.
 //  Copyright (c) 2015 Valentin Bercot & Remy Tartiere. All rights reserved.
 //
-
+#import "AppDelegate.h"
 #import "AddLanguageViewController.h"
+#import "Language+DataModel.h"
 
 // ===== DEFINITION =====
+@interface AddLanguageViewController ()
+
+// ===== PROPERTIES =====
+
+@property (weak, nonatomic) AppDelegate* appDelegate;
+
+@end
 
 @implementation AddLanguageViewController
 
@@ -17,7 +25,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // appDelegate initialization
+    self->_appDelegate = [[UIApplication sharedApplication] delegate];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,7 +46,12 @@
 
 - (IBAction)save:(id)sender
 {
-    // TODO Save in database
+    NSString *nameLanguage = self.name.text;
+    NSManagedObjectContext *context = self.appDelegate.managedObjectContext;
+    NSEntityDescription *entityDescription = [NSEntityDescription entityForName:@"Language" inManagedObjectContext:context];
+    
+    [Language getObjectWithName:nameLanguage withEntityDescription:entityDescription inManagedObjectContext:context];
+    
     [self.navigationController popViewControllerAnimated:YES];
 }
 
